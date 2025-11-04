811cd611c7ba633e3a3a03c1e5c1a4ad4adc6f02
jerrylingj <498539230@qq.com>
2025-11-04 22:10:56 +0800

    feat: init, 测试API连接

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # easy-code
+
+1. 创建虚拟环境
+
+```bash
+python -m venv venv
+```
+
+2. 安装依赖
+
+```bash
+pip install -r requirements.txt
+```
---
+++ src/main.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+def main():
+    print("=" * 60)
+    print("Easy Code - a code agent")
+    print("=" * 60)
+    print()
+
+    # 读取 api_key
+    api_key = os.getenv("DEEPSEEK_API_KEY")
+    if not api_key:
+        print("API Key 未设置")
+        return
+
+    # 初始化
+    model = ChatOpenAI(
+        model="deepseek-chat",
+        api_key=api_key,
+        base_url="https://api.deepseek.com",
+        temperature=0,
+    )
+    print("模型初始化成功")
+    print()
+
+    # 对话循环
+    print("输入 [quit] 或 [exit] , 结束对话")
+    print("-" * 60)
+    print()
+
+    while True:
+        user_input = input("你: ")
+
+        if (user_input.lower() in ['quit', 'exit']):
+            print("再见!")
+            break
+
+        # 过滤空值
+        if not user_input.strip():
+            continue
+
+        # 调用模型
+        try:
+            print("\nAgent: ", end="", flush=True)
+            
+            # 流式输出
+            for chunk in model.stream(user_input):
+                print(chunk.content, end="", flush=True)
+            
+            print("\n")  # 换行
+        except Exception as e:
+            print(f"\n错误: {e}")
+            print()
+
+if __name__ == "__main__":
+    main()
